74157492a3672b8e7b529bb94ae128e5e4528cbe
Thugg55 <thuggeru7@gmail.com>
2023-12-29 07:21:09 -0800

    completed 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,10 @@
 # function adds 1 and 2
 # Edogun ...
 
-"""print the sum of 1 and 2"""
-import add_0.add
+if __name__ == "__main__":
+    """print the sum of 1 and 2"""
+    import add_0.add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
