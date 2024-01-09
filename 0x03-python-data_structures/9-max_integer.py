74f6b0cf8013eccc19cd1df5bbcc4d74526f01f6
Thugg55 <thuggeru7@gmail.com>
2024-01-09 07:13:15 -0800

    completed

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Edogun ...
+
+def max_integer(my_list=[]):
+    if my_list:
+        new_list = [my_list[0]]
+        for i in my_list:
+            if i > new_list[0]:
+                new_list[0] = i
+        return(new_list[0])
+    return(None)
