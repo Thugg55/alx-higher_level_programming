23bc343039caefcc75c986aacc203f49c4d5d142
Thugg55 <thuggeru7@gmail.com>
2024-01-09 06:33:59 -0800

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,6 @@
 # Edogun ...
 
 def replace_in_list(my_list, idx, element):
-    if idx < o or idx > len(my_list):
-        return(my_list)
-    else:
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
-        return(my_list)
+    return (my_list)
