947b2a248d2ac08699dcc4c6ae9158e98d870dc9
Thugg55 <thuggeru7@gmail.com>
2024-01-10 04:03:53 -0800

    name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Edogun ...
+""" function prints my name """
+
+def say_my_name(first_name, last_name=""):
+    """
+    Args:
+    First_name: String contains first name.
+    Second_name: String contains second name.
+
+    Raises:
+    TypeError: If Args ain't strings
+    """
+    error = "first_name must be a string or last_name must be a string" 
+    if type(first_name) is not str or type(last_name) is not str:
+        raise TypeError(error)
+    print("My name is {} {}".format(first_name, last_name))
