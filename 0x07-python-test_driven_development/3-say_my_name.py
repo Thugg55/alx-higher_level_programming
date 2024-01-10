461c201dac1cd40c1e32c594f8dfc9597467b618
Thugg55 <thuggeru7@gmail.com>
2024-01-10 04:11:04 -0800

    pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,7 @@
 # Edogun ...
 """ function prints my name """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Args:
@@ -11,7 +12,7 @@ def say_my_name(first_name, last_name=""):
     Raises:
     TypeError: If Args ain't strings
     """
-    error = "first_name must be a string or last_name must be a string" 
+    error = "first_name must be a string or last_name must be a string"
     if type(first_name) is not str or type(last_name) is not str:
         raise TypeError(error)
     print("My name is {} {}".format(first_name, last_name))
