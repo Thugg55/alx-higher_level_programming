d834cb38a2085a050c8ad631722acd01116c90f7
Thugg55 <thuggeru7@gmail.com>
2024-03-11 15:56:00 -0700

    multi languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script prints 3 lines
+
+const myVar1, myVar2, myVar3;
+myVar1 = 'C is fun';
+myVar2 = 'Python is cool';
+myVar3 = 'JavaScript is amazing';
+console.log(myVar1\nmyVar2\nmyVar3);
