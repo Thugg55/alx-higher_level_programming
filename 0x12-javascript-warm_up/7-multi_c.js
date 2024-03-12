1663837f28ea89ffb2ae389b7aac1dc75bbdd5de
Thugg55 <thuggeru7@gmail.com>
2024-03-11 17:31:35 -0700

    multi c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script prints x times "C is fun"
+
+let i = 0;
+const {argv} = require('process');
+
+if (argv[2] === undefined)
+	console.log('Missing number of occurrences');
+else
+	for (i = 0; i < parseInt(argv[2]); i++)
+	{
+		console.log('C is fun');
+	}
