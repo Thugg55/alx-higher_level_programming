f0f7bbf7b073e98c7aee12e2e3f149fe2f0fd336
Thugg55 <thuggeru7@gmail.com>
2024-03-11 17:50:07 -0700

    add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script prinsts the addition of two numbers
+
+const { argv } = require('process);
+
+function add(a,b)
+{
+	return parseInt(a) + parseInt(b);
+};
+
+console.log(add);
