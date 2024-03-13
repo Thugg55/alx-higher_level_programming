df7159c6ffcc658d420e7fb545324737dc0069a3
Thugg55 <thuggeru7@gmail.com>
2024-03-13 14:54:30 -0700

    working on correct

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 // Script prints 3 lines
 
-const myVar1 = 'C is fun';
-const myVar2 = 'Python is cool';
-const myVar3 = 'JavaScript is amazing';
-console.log(myVar1\nmyVar2\nmyVar3);
+myvar = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(myvar);
