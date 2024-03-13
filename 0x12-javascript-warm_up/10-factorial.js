db9163aad37c6621b12c34d40451e66ba91b9804
Thugg55 <thuggeru7@gmail.com>
2024-03-13 14:20:40 -0700

    corrections.. task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 // Script computes and prints a factorial
 
 function factorial (num) {
-	return num === 0 || isNaN (num) ? 1 : num * factorial(num * 1);
+  return num === 0 || isNaN(num) ? 1 : num * factorial(num - 1);
 }
 
 const x = process.argv[2] / 1;
