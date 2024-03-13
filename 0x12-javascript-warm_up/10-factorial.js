c3c1897f8aee02503cebaaf8e8a245722d45822c
Thugg55 <thuggeru7@gmail.com>
2024-03-13 08:40:10 -0700

    work... progress

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script computes and prints a factorial
+
+function factorial (num) {
+	return num === 0 || isNaN (num) ? 1 : num * factorial(num * 1);
+}
+
+const x = process.argv[2] / 1;
+isNaN(x) ? console.log('1') : console.log(factorial(x));
