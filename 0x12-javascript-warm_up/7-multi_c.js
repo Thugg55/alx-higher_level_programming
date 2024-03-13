4f03bb17321d97b34ca8f6fd6d212cbf0385e2aa
Thugg55 <thuggeru7@gmail.com>
2024-03-13 16:04:48 -0700

    corectionsfc

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,10 @@
 // Script prints x times "C is fun"
 
 let i = 0;
-const {argv} = require('process');
+const { argv } = require('process');
 
-if (argv[2] === undefined)
-	console.log('Missing number of occurrences');
-else
-	for (i = 0; i < parseInt(argv[2]); i++)
-	{
-		console.log('C is fun');
-	}
+if (argv[2] === undefined) { console.log('Missing number of occurrences'); } else {
+  for (i = 0; i < parseInt(argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
