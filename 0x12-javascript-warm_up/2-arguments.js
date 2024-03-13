c39479bbc7b58692a65313a63ab6bcad049537df
Thugg55 <thuggeru7@gmail.com>
2024-03-13 15:11:13 -0700

    completed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,7 @@
 // prints a message based on the number of arguments
 
 let myVar;
-for (myVar in process.argv) {
-}
-myVar--;
-if (myVar === 0)
-	console.log('No argument');
-if (myVar === 1)
-	console.log('Argument found');
-if (myVar > 1)
-	console.log('Arguments found');
+for (myVar in process.argv) { myVar--; }
+if (myVar === 0) { console.log('No argument'); }
+if (myVar === 1) { console.log('Argument found'); }
+if (myVar > 1) { console.log('Arguments found'); }
