0ed203531f72532053f372b07583b7f9c2feee6d
Thugg55 <thuggeru7@gmail.com>
2024-03-12 19:40:48 -0700

    working...

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // a class Square that extends from a rectangle class
 
-const Rectangle = require('./4-rectangle')
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
 	constructor (size) {
