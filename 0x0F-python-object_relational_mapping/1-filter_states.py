4eb10041f2ff1dc0363cb27907c196126a342cd2
Thugg55 <thuggeru7@gmail.com>
2024-03-16 16:49:17 -0700

    work in progress :

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
             host='localhost', port=3306)
     curs = db.cursor()
     curs.execute("SELECT * FROM `states` ORDER BY states.id")
-    [print(state) for state in curs.fetchall()if state[1][0] == "N"]
+    [print(state) for state in curs.fetchall() if state[1][0] == "N"]
