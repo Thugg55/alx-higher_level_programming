778542c3923767eb008e66915dd3bd86665f1438
Thugg55 <thuggeru7@gmail.com>
2024-03-16 19:20:27 -0700

    task 7 work corrections

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script lists all state objects form the database """
+"""Script lists all state objects form the database"""
 
 import sys
 from model_state import Base, State
@@ -9,10 +9,10 @@ from sqlalchemy import create_engine
 if __name__ = "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
              sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    #engine created
+    # engine created
     Session = sessionmaker(bind=engine)
     session = Session()
-    #session created
+    # session created
 
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
