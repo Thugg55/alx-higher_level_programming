204bc179f05c66da1950f9edeb9811772719f131
Thugg55 <thuggeru7@gmail.com>
2024-03-28 06:03:36 -0700

    correction

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
 # Check if URL argument is provided
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
-
 url=$1
 email="test@gmail.com"
-subt="I will always be here for PLD"
-
+subject="I will always be here for PLD"
 # Send POST request with specific parameters and display the response body
-response=$(curl -s -X POST "$url" -d "email=$email" -d "subjt=$subject")
+response=$(curl -s -X POST "$url" -d "email=$email" -d "subject=$subject")
 echo "$response"
 
