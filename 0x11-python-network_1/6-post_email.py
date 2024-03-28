3d87078f713d93d5adfe327a3f7ad6e5d46949ee
Thugg55 <thuggeru7@gmail.com>
2024-03-28 10:31:55 -0700

    pycode correct

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Script takes in a URL and an email address, sends a POST request to the URL with 
-the email as parameter, and finally displays the body of the response"""
+"""Script takes in a URL and an email address, sends a POST request to the
+URL with the email as parameter, and finally displays the body of the
+response"""
 
 import requests
 import sys
