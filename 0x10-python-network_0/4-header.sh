af451ee0af45c317c505b6fd3ab82fa54797beb3
Thugg55 <thuggeru7@gmail.com>
2024-03-29 13:40:17 -0700

    trying to corrextv task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script takes in a URL as an argument
-# Sends a GET request to the URL and displays the body 
+# Sends a GET request to the URL and displays the body
 curl -sLH "X-School-User-Id: 98" "$1"
