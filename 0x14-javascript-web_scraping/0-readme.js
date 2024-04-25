6cf0a5ef5f0f2f0bd1a9f3a4a8d3aa30aace8c27
Thugg55 <100637930+Thugg55@users.noreply.github.com>
2024-04-25 02:58:51 +0100

    Update 0-readme.js
    task 0 on it

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,4 +2,22 @@
 /* Script reads and prints the content of a file */
 
 const fs = require('fs');
-const args = process.argv[2];
+const filePath = process.argv[2];
+
+function readFile(filePath) {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error("An error occurred while reading the file:");
+            console.error(err);
+            return;
+        }
+        console.log(data);
+    });
+}
+
+if (process.argv.length !== 3) {
+    console.log("Usage: node script.js <file_path>");
+} else {
+    readFile(filePath);
+}
+
